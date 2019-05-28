21797154455d80251bad6b3e81ddf74ea90d3f2a
crazy <1325557261@qq.com>
2019-05-28 09:36:32 +0800

    编写的品牌审核、规格审核，用逆向工程生成了pojo、dao接口、dao.xml，数据库创建了新表，在web-manager-adimd中添加了brand_check，并修改了相应的js，html页面；在web-manager中的java-controller中添加了BrandCheckController，并添加了想对应的接口、实现类。

--- pinyougou-parent/pinyougou-pojo/src/main/java/vo/SpecificationVo.java
+++ pinyougou-parent/pinyougou-pojo/src/main/java/vo/SpecificationVo.java
@@ -1,7 +1,9 @@
 package vo;
 
 import cn.itcast.core.pojo.specification.Specification;
+import cn.itcast.core.pojo.specification.SpecificationCheck;
 import cn.itcast.core.pojo.specification.SpecificationOption;
+import cn.itcast.core.pojo.specification.SpecificationOptionCheck;
 
 import java.io.Serializable;
 import java.util.List;
@@ -14,8 +16,29 @@ public class SpecificationVo implements Serializable{
 
     //规格对象 1
     private Specification specification;
+
+    private SpecificationCheck specificationCheck;
     //规格选项结果集对象 多
     private List<SpecificationOption> specificationOptionList;
+
+    private List<SpecificationOptionCheck> specificationOptionCheckList;
+
+
+    public SpecificationCheck getSpecificationCheck() {
+        return specificationCheck;
+    }
+
+    public void setSpecificationCheck(SpecificationCheck specificationCheck) {
+        this.specificationCheck = specificationCheck;
+    }
+
+    public List<SpecificationOptionCheck> getSpecificationOptionCheckList() {
+        return specificationOptionCheckList;
+    }
+
+    public void setSpecificationOptionCheckList(List<SpecificationOptionCheck> specificationOptionCheckList) {
+        this.specificationOptionCheckList = specificationOptionCheckList;
+    }
 
     public Specification getSpecification() {
         return specification;
